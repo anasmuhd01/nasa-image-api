9717fd3fe2e61f5e969e380424f8b660b9d53d0d
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-16 13:53:58 +0530

    type changed to form

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 import streamlit as st
 
 
-url = "https://api.nasa.gov/planetary/apod?api_key=gnoCmPes81gIQ7NhVkSq8EVHp9xNx5qoMhgqLqFb"
-content = requests.get(url)
-file = content.json()
+with st.form(key="nasa"):
+    date = st.text_input("enter date:")
+    btn = st.form_submit_button("submit")
+    if btn:
+        print(date)
+        url = f"https://api.nasa.gov/planetary/apod?api_key=gnoCmPes81gIQ7NhVkSq8EVHp9xNx5qoMhgqLqFb&date={date}"
+        content = requests.get(url)
+        file = content.json()
 
-
-st.title(file["title"])
-st.image(file["url"])
-st.caption(file["explanation"])
-
-# print(file)
+        st.title(file["title"])
+        st.image(file["url"])
+        st.caption(file["explanation"])
