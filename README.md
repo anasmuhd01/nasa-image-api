f9c92e5c1f683f3e8e6c773b31a67081772b3a09
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-16 14:05:11 +0530

    README.md added

---
+++ README.md
@@ -0,0 +1,6 @@
+# APOD - Astronomy Picture of the Day
+this is a project built on python with nasa api - 'api.nasa.org'
+
+how to use -
+just enter year month date click submit APOD of the day you
+enterd will display there
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with st.form(key="nasa"):
     date = st.text_input("enter date : format - YYYY-MM-DD ",placeholder="YYYY-MM-DD")
     btn = st.form_submit_button("submit")
     if btn:
-        print(date)
+        # print(date)
         url = f"https://api.nasa.gov/planetary/apod?api_key=gnoCmPes81gIQ7NhVkSq8EVHp9xNx5qoMhgqLqFb&date={date}"
         content = requests.get(url)
         file = content.json()
