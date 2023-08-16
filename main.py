34af503dc0d4b14e72b549f72bc2d8a768e947da
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-16 13:32:53 +0530

    title,image,description added

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ url = "https://api.nasa.gov/planetary/apod?api_key=gnoCmPes81gIQ7NhVkSq8EVHp9xNx
 content = requests.get(url)
 file = content.json()
 
-st.title("testing")
+
+st.title(file["title"])
+st.image(file["url"])
+st.caption(file["explanation"])
 
 # print(file)
